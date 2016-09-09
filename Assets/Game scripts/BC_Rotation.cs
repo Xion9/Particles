be7dac65fa3210edb012828b0719144b192b8a8b
Xion Nine <akirameagashira@gmail.com>
2016-09-09 16:49:53 -0700

    particles
    pew pew pew

---
+++ Assets/Game scripts/BC_Rotation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Rotate the game object at a set speed on a timmer.
+ * 
+ * @author j@gamemechanix.io
+ * @project SpellCraft
+ * @copyright GameMechanix.io 2016
+ **/
+
+
+public class BC_Rotation : MonoBehaviour
+{
+
+    public Vector3 speed = Vector3.up;
+    public bool isTurning = true;
+    public int counter = 50;
+  
+    void FixedUpdate ()
+    {
+        while (isTurning)
+        {
+           transform.Rotate(speed);
+            counter--;
+        }
+        if (counter > 0)
+        {
+           transform.Rotate(new Vector3(0.0f, 0.0f, 0.0f));
+            isTurning = false;
+        }
+
+       
+            
+            //}
+            //else
+            //{
+              //  counter--;
+            } 
+    }
+
